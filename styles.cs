c5c19c87cb81482502840f1cc23645d6833d4b78
souzakaua7 <00001121514480sp@al.educacao.sp.gov.br>
2024-08-26 21:04:12 -0300

    Primeira versão do codigo

---
+++ styles.cs
@@ -0,0 +1,56 @@
+body {
+    color: white;
+    background: black;
+    margin: 0px;
+    font-family: "Chakra Petch", sans-serif;
+  }
+  
+  header {
+    border-bottom: solid 2px rgb(42, 122, 228);
+    padding: 20px;
+    font-size: 32px;
+    color: rgb(42, 122, 228);
+  }
+  
+  section {
+    background: rgb(184, 156, 213);
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+  }
+  .categoria {
+    background: rgb(27, 26, 27);
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+  }
+  h1 {
+    font-size: 40px;
+  }
+  
+  p {
+
+    font-size: 20px;
+  }
+  img {
+    height: 200px;
+  }
+  .categoria-videos {
+    display: flex;
+    overflow-x: auto;
+    gap: 10px;
+  }
+  
+  section {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  /* código omitido */
+
+.categoria {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
